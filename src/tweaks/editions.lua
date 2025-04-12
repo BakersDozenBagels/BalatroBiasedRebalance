67262e9ee7f34213749dbe4fe63f040019b003d5
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-04-12 01:19:04 -0400

    I'm good at git I promise

---
+++ src/tweaks/editions.lua
@@ -0,0 +1,28 @@
+--[[
+
+Copyright (C) 2025  BakersDozenBagels
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+]] --
+
+local raw_get_weight = G.P_CENTERS.e_negative.get_weight
+SMODS.Edition:take_ownership("negative", {
+    get_weight = function(self)
+        if G.GAME.selected_back.effect.center.key == "b_black" then
+            return raw_get_weight(self) * 4
+        end
+        return raw_get_weight(self)
+    end,
+})
